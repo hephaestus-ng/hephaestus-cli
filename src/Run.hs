c862371aaedb1a06315c5df761f41fd28f37ea05
thi4go <thiagofigueiredo777@gmail.com>
2018-05-05 00:26:28 -0300

    correcting funcs origin files

--- src/Run.hs
+++ src/Run.hs
@@ -1,41 +1,24 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts #-}
 
 module Run where
 
-import Types.Error
-import Types.State
-
-import Class.Parser
-
-import Action.Help
-import Action.Load
+import Control.Monad.State
 
 import Types.Hephaestus
+import Types.State
 
-import Data.FM.Types
-import Data.SC.Types
-import Data.SPL
-
-import Control.Monad.State
-import Control.Lens
+import Run
 
 
+runHephaestus :: Env -> Hephaestus a -> IO a
+runHephaestus v (Hephaestus m) = evalStateT m v
 
-shell :: (MonadState Env m,
-         MonadParser (ConfigurationKnowledge ComponentModel) m,
-         MonadParser FeatureModel m,
-         MonadIO m)
-         => m ()
-shell = welcome >> shellLoop
-  where
-    shellLoop = do
-      cmd <- liftIO $ getLine
-      env <- get
-      case cmd of
-        "help"    -> help
-        "load fm" -> load "fm"
-        "load ck" -> load "ck"
-        "show env" -> liftIO $ print env
 
-        "clear env" -> modify (\env -> env { _fm = Nothing, _asset = Nothing})
-      shellLoop
+run = runHephaestus initEnv shell
+
+
+initEnv = Env Nothing Nothing
+
+
+--
--- src/Shell.hs
+++ src/Shell.hs
@@ -1,24 +1,42 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
+
 {-# LANGUAGE FlexibleContexts #-}
 
 module Shell where
 
-import Control.Monad.State
-
-import Types.Hephaestus
+import Types.Error
 import Types.State
 
-import Run
+import Class.Parser
+
+import Action.Help
+import Action.Load
+
+import Types.Hephaestus
+
+import Data.FM.Types
+import Data.SC.Types
+import Data.SPL
+
+import Control.Monad.State
+import Control.Lens
 
 
-runHephaestus :: Env -> Hephaestus a -> IO a
-runHephaestus v (Hephaestus m) = evalStateT m v
 
+shell :: (MonadState Env m,
+         MonadParser (ConfigurationKnowledge ComponentModel) m,
+         MonadParser FeatureModel m,
+         MonadIO m)
+         => m ()
+shell = welcome >> shellLoop
+  where
+    shellLoop = do
+      cmd <- liftIO $ getLine
+      env <- get
+      case cmd of
+        "help"    -> help
+        "load fm" -> load "fm"
+        "load ck" -> load "ck"
+        "show env" -> liftIO $ print env
 
-run = runHephaestus initEnv shell
-
-
-initEnv = Env Nothing Nothing
-
-
---
+        "clear env" -> modify (\env -> env { _fm = Nothing, _asset = Nothing})
+      shellLoop
